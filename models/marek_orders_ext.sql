619fa5ddfcab21a2cc3c907639287820d1edc25a
puikmarek@gmail.com <puikmarek@gmail.com>
2024-05-15 07:23:02 +0000

    order and parts were swapped

--- models/marek_orders_ext.sql
+++ models/marek_orders_ext.sql
@@ -1,75 +1,197 @@
 with source_data as (
     select
-        order_part_id,
         order_id,
-        selected_process_type,
-        material_name,
-        material_type,
-        weight_g,
-        quantity,
-        manufacturer_price_eur,
-        has_bending,
-        has_surface_coating,
-        has_insert_operations,
-        bends_count,
-        created_at
-    from analytics_engineer_test_task.parts 
+        status,
+        is_cancelled,
+        customer_id,
+        manufacturer_id,
+        customer_price,
+        manufacturer_price,
+        shipping_price,
+        markup,
+        account_manager_country,
+        created_at,
+        in_production_at
+    from analytics_engineer_test_task.orders 
 ),
 
-surface_finish as (
-    select
-        order_part_id,
-        JSON_EXTRACT_SCALAR(process_config, '$.value') as surface_finish
-    from analytics_engineer_test_task.parts_surface_finish_config
-    where process_name = 'SURFACE_FINISH'
+parts_count as (
+    select 
+        order_id,
+        COUNT(DISTINCT order_part_id) as parts_count
+    from analytics_engineer_test_task.parts
+    group by order_id
+
 ),
 
-secondary_surface_finish as (
-    select
-        order_part_id,
-        JSON_EXTRACT_SCALAR(process_config, '$.value') as secondary_surface_finish
-    from analytics_engineer_test_task.parts_surface_finish_config
-    where process_name = 'SECONDARY_SURFACE_FINISH'
+cnc_count as ( -- assuming again different parts not total quantity, otherwise SUM(QUANTITY)
+    select 
+        order_id,
+        COUNT(DISTINCT order_part_id) as parts_count
+    from analytics_engineer_test_task.parts
+    where selected_process_type = 'cnc_machining'
+    group by order_id
 ),
 
-ral as (
-    select
-        order_part_id,
-        JSON_EXTRACT_SCALAR(process_config, '$.ralCode') as ral_code,
-        JSON_EXTRACT_SCALAR(process_config, '$.ralFinish') as ral_finish
-    from analytics_engineer_test_task.parts_surface_finish_config
-    where process_name = 'SECONDARY_SURFACE_FINISH_RAL'
+laser_count as ( -- assuming again different parts not total quantity, otherwise SUM(QUANTITY)
+    select 
+        order_id,
+        COUNT(DISTINCT order_part_id) as parts_count
+    from analytics_engineer_test_task.parts
+    where selected_process_type in ('laser_cutting','laser_tube_cutting')
+    group by order_id
+),
+
+bending_parts as ( -- assuming again different parts not total quantity, otherwise SUM(QUANTITY)
+    select 
+        order_id,
+        COUNT(DISTINCT order_part_id) as parts_count,
+        SUM(bends_count) as bends_count --assuming again we dont take quantity into consideration, otherwise SUM(bends_count * quantity)
+    from analytics_engineer_test_task.parts
+    where has_bending = 1
+    group by order_id
+),
+
+surface_coating_count as ( -- assuming again different parts not total quantity, otherwise SUM(QUANTITY)
+    select 
+        order_id,
+        COUNT(DISTINCT order_part_id) as parts_count
+    from analytics_engineer_test_task.parts
+    where has_surface_coating = true
+    group by order_id
+),
+
+insert_operations_count as ( -- assuming again different parts not total quantity, otherwise SUM(QUANTITY)
+    select 
+        order_id,
+        COUNT(DISTINCT order_part_id) as parts_count
+    from analytics_engineer_test_task.parts
+    where has_insert_operations = true
+    group by order_id
+),
+/*
+unable to select from {{ ref('marek_parts_ext') }} so i am adding the same cte here.
+*/
+ral_code_list as (
+    select 
+        sub.order_id,
+        STRING_AGG(sub.ral_code) as ral_code_list
+    from (
+        select distinct
+            parts.order_id,
+            JSON_EXTRACT_SCALAR(process_config, '$.ralCode') as ral_code
+        from analytics_engineer_test_task.parts_surface_finish_config as ral
+        inner join analytics_engineer_test_task.parts as parts on 
+            parts.order_part_id = ral.order_part_id
+        where process_name = 'SECONDARY_SURFACE_FINISH_RAL'
+    ) sub
+    group by sub.order_id
+),
+
+ral_finish_list as (
+    select 
+        sub.order_id,
+        STRING_AGG(sub.ral_finish) as ral_finish_list
+    from (
+        select distinct
+            parts.order_id,
+            JSON_EXTRACT_SCALAR(process_config, '$.ralFinish') as ral_finish
+        from analytics_engineer_test_task.parts_surface_finish_config as ral
+        inner join analytics_engineer_test_task.parts as parts on 
+            parts.order_part_id = ral.order_part_id
+        where process_name = 'SECONDARY_SURFACE_FINISH_RAL'
+    ) sub 
+    group by sub.order_id
+),
+
+surface_finish_list as (
+    select 
+        sub.order_id,
+        STRING_AGG(sub.surface_finish) as surface_finish_list
+    from (
+        select distinct
+            parts.order_id,
+            JSON_EXTRACT_SCALAR(process_config, '$.value') as surface_finish
+        from analytics_engineer_test_task.parts_surface_finish_config as ral
+        inner join analytics_engineer_test_task.parts as parts on 
+            parts.order_part_id = ral.order_part_id
+        where process_name = 'SURFACE_FINISH'
+    ) sub 
+    group by sub.order_id
+),
+
+secondary_surface_finish_list as (
+    select 
+        sub.order_id,
+        STRING_AGG(sub.surface_finish) as secondary_surface_finish_list
+    from (
+        select distinct
+            parts.order_id,
+            JSON_EXTRACT_SCALAR(process_config, '$.value') as surface_finish
+        from analytics_engineer_test_task.parts_surface_finish_config as ral
+        inner join analytics_engineer_test_task.parts as parts on 
+            parts.order_part_id = ral.order_part_id
+        where process_name = 'SECONDARY_SURFACE_FINISH'
+    ) sub 
+    group by sub.order_id
 ),
 
 final as (
-    select 
-        parts.order_part_id,
-        parts.order_id,
-        parts.selected_process_type,
-        parts.material_name,
-        parts.material_type,
-        parts.weight_g,
-        parts.quantity,
-        parts.manufacturer_price_eur,
-        parts.has_bending,
-        parts.has_surface_coating,
-        parts.has_insert_operations,
-        parts.bends_count,
-        parts.created_at,
-        sf.surface_finish,
-        ssf.secondary_surface_finish,
-        ral.ral_code,
-        ral.ral_finish
-    from source_data parts
+    select
+        source_data.order_id,
+        source_data.status,
+        source_data.is_cancelled,
+        source_data.customer_id,
+        source_data.manufacturer_id,
+        source_data.customer_price,
+        source_data.manufacturer_price,
+        source_data.shipping_price,
+        source_data.markup,
+        source_data.account_manager_country,
+        source_data.created_at,
+        source_data.in_production_at,
+        parts_count.parts_count,
+        cnc_count.parts_count as cnc_parts_count,
+        laser_count.parts_count as laser_parts_count,
+        bending_parts.parts_count as bending_parts_count,
+        bending_parts.bends_count as total_bends_count,
+        surface_coating_count.parts_count as surface_coating_parts_count,
+        insert_operations_count.parts_count as insert_operations_parts_count,
+        ral_code_list.ral_code_list,
+        ral_finish_list.ral_finish_list,
+        surface_finish_list.surface_finish_list,
+        secondary_surface_finish_list.secondary_surface_finish_list
 
-    left join surface_finish sf on
-        sf.order_part_id = parts.order_part_id
+    from source_data source_data 
 
-    left join secondary_surface_finish ssf on
-        ssf.order_part_id = parts.order_part_id
+    left join parts_count parts_count on
+        source_data.order_id = parts_count.order_id
 
-    left join ral ral on
-        ral.order_part_id = parts.order_part_id
+    left join cnc_count cnc_count on 
+        source_data.order_id = cnc_count.order_id
+        
+    left join laser_count laser_count on 
+        source_data.order_id = laser_count.order_id
+    
+    left join bending_parts bending_parts on 
+        source_data.order_id = bending_parts.order_id
+            
+    left join surface_coating_count surface_coating_count on 
+        source_data.order_id = surface_coating_count.order_id
+            
+    left join insert_operations_count insert_operations_count on 
+        source_data.order_id = insert_operations_count.order_id
+            
+    left join ral_code_list ral_code_list on 
+        source_data.order_id = ral_code_list.order_id
+            
+    left join ral_finish_list ral_finish_list on 
+        source_data.order_id = ral_finish_list.order_id
+            
+    left join surface_finish_list surface_finish_list on 
+        source_data.order_id = surface_finish_list.order_id
+            
+    left join secondary_surface_finish_list secondary_surface_finish_list on 
+        source_data.order_id = secondary_surface_finish_list.order_id
 )
-
 select * from final
